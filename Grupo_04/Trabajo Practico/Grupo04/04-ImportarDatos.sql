01a113eaa6e1256e267a99aa6a98f2ebba7c04f8
ezequielmp <ezequielmunozpalazzo@gmail.com>
2024-11-29 12:06:04 -0300

    Notas de créditos

--- Grupo_04/Trabajo Practico/Grupo04/04-ImportarDatos.sql
+++ Grupo_04/Trabajo Practico/Grupo04/04-ImportarDatos.sql
@@ -12,25 +12,25 @@ reconfigure;
 GO
 
 --habilito consultas distribuidas, para poder usar la funcion openrowset
-/*
+
 sp_configure 'show advanced options', 1;
 RECONFIGURE;
 GO
 sp_configure 'Ad Hoc Distributed Queries', 1;
 RECONFIGURE;
 GO
-*/
+
 
 
 --NECESARIO PARA PODER IMPORTAR DESDE XLSX
-/*
+
 USE [master] 
 GO 
 EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
 GO 
 EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
 GO 
-*/
+
 --FUNCIÓN QUE DEVUELVE EL PRECIO DEL DÓLAR OFICIAL
 CREATE OR ALTER FUNCTION CATALOGO.PRECIO_DOLAR() RETURNS NUMERIC(10, 2) AS
 BEGIN
